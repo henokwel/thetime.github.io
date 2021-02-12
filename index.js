b8bc3f013cbc3aa983e8223ff163c6455fb98aad
henokwel <hen.ok@live.com>
2021-02-12 05:22:41 +0100

    package adjusting

--- index.js
+++ index.js
@@ -1,19 +1,4 @@
 $(document).ready((e) => {
-    console.log(navigator.geolocation);
-
-
-
-    // let zones = moment.tz.zonesForCountry("SE")
-    // let hour = moment().tz(zones).format('HH')
-    // let min = moment().tz(zones).format('mm')
-    // $("#hour").text(hour)
-    // $("#min").text(min)
-    // $(".country").text(region)
- 
-
-    let hours = document.querySelector("#hours")
-    let minut = document.querySelector("#min")
-
 
     $("#vmap").vectorMap({
         map: 'world_en',
@@ -23,31 +8,22 @@ $(document).ready((e) => {
         color: '#797979',
 
         onRegionClick: function (element, code, region) {
-            console.log(code);  //SE
-            console.log(region); // Sweden
-
 
             let zones = moment.tz.zonesForCountry(code)[0]
-
             let hour = moment().tz(zones).format('HH')
             let min = moment().tz(zones).format('mm')
             $("#hour").text(hour)
             $("#min").text(min)
             $(".country").text(region)
-        }
-        ,
-        onLoad:function(){
+        },
+        onLoad: function () {
             let zones = moment.tz.zonesForCountry("SE")[0]
             let hour = moment().tz(zones).format('HH')
             let min = moment().tz(zones).format('mm')
-            console.log(min);
-            
             $("#hour").text(hour)
             $("#min").text(min)
             $(".country").text("Sweden")
         }
     });
-
-
 
 })

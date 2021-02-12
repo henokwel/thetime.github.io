9c4c6e78440796bf9322e8f890ba3eefb7bb80ca
henokwel <hen.ok@live.com>
2021-02-12 03:07:59 +0100

    clearning up

---
+++ index.js
@@ -0,0 +1,27 @@
+$(document).ready((e) => {
+    console.log(navigator.geolocation);
+    
+    
+    
+    let hours = document.querySelector("#hours")
+    let minut = document.querySelector("#min")
+    const vMap = document.querySelector("#vmap")
+    
+    $("#vmap").vectorMap({
+        map: 'world_en',
+        selectedColor: '#ffff',
+        backgroundColor: "none",
+        hoverColor: '#FFE27D',
+        color: '#797979',
+
+        onRegionClick: function (element, code, region) {
+            let zones = moment.tz.zonesForCountry(code)[0]
+            let hour = moment().tz(zones).format('HH')
+            let min = moment().tz(zones).format('mm')
+            $("#hour").text(hour)
+            $("#min").text(min)
+            $(".country").text(region)
+        }
+    });
+
+})

fc7ce6229a56417405a679f1fd842d5b37c7e073
henokwel <hen.ok@live.com>
2021-02-12 05:18:51 +0100

    onload config

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ $(document).ready((e) => {
 
 
 
+    // let zones = moment.tz.zonesForCountry("SE")
+    // let hour = moment().tz(zones).format('HH')
+    // let min = moment().tz(zones).format('mm')
+    // $("#hour").text(hour)
+    // $("#min").text(min)
+    // $(".country").text(region)
+ 
+
     let hours = document.querySelector("#hours")
     let minut = document.querySelector("#min")
 
@@ -15,15 +23,31 @@ $(document).ready((e) => {
         color: '#797979',
 
         onRegionClick: function (element, code, region) {
+            console.log(code);  //SE
+            console.log(region); // Sweden
+
+
             let zones = moment.tz.zonesForCountry(code)[0]
+
             let hour = moment().tz(zones).format('HH')
             let min = moment().tz(zones).format('mm')
             $("#hour").text(hour)
             $("#min").text(min)
             $(".country").text(region)
         }
+        ,
+        onLoad:function(){
+            let zones = moment.tz.zonesForCountry("SE")[0]
+            let hour = moment().tz(zones).format('HH')
+            let min = moment().tz(zones).format('mm')
+            console.log(min);
+            
+            $("#hour").text(hour)
+            $("#min").text(min)
+            $(".country").text("Sweden")
+        }
     });
 
- 
+
 
 })
